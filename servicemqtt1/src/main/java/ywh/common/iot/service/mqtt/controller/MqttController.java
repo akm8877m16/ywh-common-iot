9cab038f1a54ce1c74e496ebb79f7b0ae52a504f
zhangmin <zhangmin@gegolab.com>
2018-06-01 11:38:36 +0800

    0601 update0601 update0601 update0601 update0601 update

--- servicemqtt1/src/main/java/ywh/common/iot/service/mqtt/controller/MqttController.java
+++ servicemqtt1/src/main/java/ywh/common/iot/service/mqtt/controller/MqttController.java
@@ -1,22 +1,17 @@
 package ywh.common.iot.service.mqtt.controller;
 
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 //import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import springfox.documentation.annotations.ApiIgnore;
+import ywh.common.bean.MqttPublishBean;
 import ywh.common.iot.service.mqtt.mqtt.MqttParameters;
 import ywh.common.iot.service.mqtt.mqtt.Publisher;
 import ywh.common.mqtt.MQTTPublisher;
@@ -62,22 +57,14 @@ public class MqttController {
 
     /**
      *
-     * @param deviceSn
-     * @param request
-     * @return
-     *
      */
     @ApiOperation(value = "遥控指定设备",notes = "目前只有拥有ROLE_USER权限的用户才能遥控属于自己的设备")
-    @PostMapping(path = "/control/{deviceSn}")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "deviceSn", value = "设备唯一标识Sn", required = true, dataType = "String",paramType="path"),
-            @ApiImplicitParam(name = "attribute", value = "需要遥控的设备topic属性", required = true, dataType = "String",paramType="form"),
-            @ApiImplicitParam(name = "payload", value = "设备遥控指令", required = true, dataType = "String",paramType="form")
-    })
+    @PostMapping(path = "/control")
     @CheckMqttDeviceBinding
-    public Msg sendRemoteControl(@PathVariable String deviceSn, Principal principal, HttpServletRequest request){
-        String attribute = request.getParameter("attribute");
-        String payload = request.getParameter("payload");
+    public Msg sendRemoteControl(@RequestBody @ApiParam(name="Mqtt发布对象",value="传入json格式,都必填",required=true) MqttPublishBean mqttPublishBean, Principal principal){
+        String attribute = mqttPublishBean.getAttribute();
+        String payload = mqttPublishBean.getPayload();
+        String deviceSn = mqttPublishBean.getSn();
         String realTopic = mqttRootTopic + "/" + deviceSn + "/" + attribute;
         mqttPublisher.publishMessage(realTopic,payload);
         return ResultUtil.success();
